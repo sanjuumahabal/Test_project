434905c483cddfbc36a7c7a71941af92812006b9
sanjuumahabal <168001917+sanjuumahabal@users.noreply.github.com>
2025-02-06 15:09:02 +0530

    Update addTwoNumbers.py

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,4 +1,6 @@
-print("Program for Adding Two Numbers")
-num1 = int(input("Enter First Number: "))
-num2 = int(input("Enter Second Number: "))
-print(f"Sum if {num1+num2}")
+import os
+
+num1 = int(os.getenv("NUM1", 0))  # Default value is 0 if NUM1 is not set
+num2 = int(os.getenv("NUM2", 0))  # Default value is 0 if NUM2 is not set
+
+print(f"Sum: {num1 + num2}")
